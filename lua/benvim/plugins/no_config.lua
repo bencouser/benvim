03d317a1fd4bfd5a653356bb1e359858fe053ae1
bencouser <neb2010@live.co.uk>
2024-02-28 11:48:39 +0000

    mainly transfered all the things I liked from kickstart

---
+++ lua/benvim/plugins/no_config.lua
@@ -0,0 +1,8 @@
+return {
+	"tpope/vim-sleuth", -- Detect tabstop and shiftwidth automatically
+
+	-- "gc" to comment visual regions/lines
+	{ "numToStr/Comment.nvim", opts = {} },
+
+	"github/copilot.vim",
+}
--- lua/benvim/settings.lua
+++ lua/benvim/settings.lua
@@ -60,3 +60,15 @@ vim.opt.swapfile = false
 vim.opt.backup = false
 vim.opt.undodir = os.getenv("HOME") .. "/.vim/undodir"
 vim.opt.undofile = true
+
+-- Highlight when yanking (copying) text
+--  Try it with `yap` in normal mode
+--  See `:help vim.highlight.on_yank()`
+vim.api.nvim_create_autocmd('TextYankPost', {
+  desc = 'Highlight when yanking (copying) text',
+  group = vim.api.nvim_create_augroup('kickstart-highlight-yank', { clear = true }),
+  callback = function()
+    vim.highlight.on_yank()
+  end,
+})
+
