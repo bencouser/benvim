c0fdb9040bc8a3bf4fec1725c36268f6071dd744
bencouser <neb2010@live.co.uk>
2024-03-23 21:04:51 +0000

    I am a jedi

--- lua/benvim/init.lua
+++ lua/benvim/init.lua
@@ -1,7 +1,7 @@
 -- Set Leader to Space
-vim.g.mapleader = ' '
-vim.g.maplocalleader = ' '
+vim.g.mapleader = " "
+vim.g.maplocalleader = " "
 
-require('benvim.lazy_config')
-require('benvim.remaps')
-require('benvim.settings')
+require("benvim.lazy_config")
+require("benvim.remaps")
+require("benvim.settings")
